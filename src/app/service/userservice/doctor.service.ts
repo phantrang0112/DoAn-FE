8592c81766b407f1f96f70df94889577255500f9
TrangPT <trangpt@itsj-group.com.vn>
2022-05-30 17:42:31 +0700

    updtae-user

---
+++ src/app/service/userservice/doctor.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DoctorService {
+  listDoctor;
+  listDept;
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      // 'Authorization': 'Bearer ' + this.token,
+      // "Content-Type": "multipart/form-data",
+      // 'Accept': 'application/json',
+      // 'Content-Type': 'application/json',
+    }),
+  };
+  constructor(private httpclient: HttpClient,) { }
+  public getListDoctor(): Observable<any> {
+    const url = `${environment.doctorURL}`;
+    return this.httpclient.get<any>(url, this.httpOptions);// Nhớ import catchError
+  }
+}
