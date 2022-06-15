d71bdaea861bc7467432386d410e787ea97c6f34
TrangPT <trangpt@itsj-group.com.vn>
2022-06-16 00:44:32 +0700

    update Shedule

--- src/app/user/appointment-schedule/registration-schedule/registration-schedule.component.ts
+++ src/app/user/appointment-schedule/registration-schedule/registration-schedule.component.ts
@@ -1,10 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {Router} from '@angular/router';
-import {DeptService} from 'src/app/service/userservice/dept.service';
-import {DoctorService} from 'src/app/service/userservice/doctor.service';
-import {HeaderserviceService} from 'src/app/service/userservice/headerservice.service';
-import {PaymentService} from 'src/app/service/userservice/paymentservice.service';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { appointmentSchedule } from 'src/app/models/appointment-schedule';
+import { UserserviceService } from 'src/app/service/userservice.service';
+import { DeptService } from 'src/app/service/userservice/dept.service';
+import { DoctorService } from 'src/app/service/userservice/doctor.service';
+import { HeaderserviceService } from 'src/app/service/userservice/headerservice.service';
+import { PaymentService } from 'src/app/service/userservice/paymentservice.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -14,45 +16,46 @@ import Swal from 'sweetalert2';
 })
 export class RegistrationScheduleComponent implements OnInit {
   time = [
-    { time: '07:00', class: ''},
-    { time: '07:30', class: ''},
-    { time: '08:00', class: ''},
-    { time: '08:30', class: ''},
-    { time: '09:00', class: ''},
-    { time: '09:30', class: ''},
-    { time: '10:00', class: ''},
-    { time: '10:30', class: ''},
-    { time: '11:00', class: ''},
-    { time: '11:30', class: ''},
-    { time: '12:00', class: ''},
-    { time: '12:30', class: ''},
-    { time: '13:00', class: ''},
-    { time: '13:30', class: ''},
+    { time: '07:00', class: '' },
+    { time: '07:30', class: '' },
+    { time: '08:00', class: '' },
+    { time: '08:30', class: '' },
+    { time: '09:00', class: '' },
+    { time: '09:30', class: '' },
+    { time: '10:00', class: '' },
+    { time: '10:30', class: '' },
+    { time: '11:00', class: '' },
+    { time: '11:30', class: '' },
+    { time: '12:00', class: '' },
+    { time: '12:30', class: '' },
+    { time: '13:00', class: '' },
+    { time: '13:30', class: '' },
 
-    { time: '14:00', class: ''},
-    { time: '14:30', class: ''},
-    { time: '15:00', class: ''},
-    { time: '15:30', class: ''},
-    { time: '16:00', class: ''},
-    { time: '16:30', class: ''},
-    { time: '17:00', class: ''},
-    { time: '17:30', class: ''},
-    { time: '18:30', class: ''},
-    { time: '19:00', class: ''},
-    { time: '19:30', class: ''},
-    { time: '20:00', class: ''},
-    { time: '20:30', class: ''},]
+    { time: '14:00', class: '' },
+    { time: '14:30', class: '' },
+    { time: '15:00', class: '' },
+    { time: '15:30', class: '' },
+    { time: '16:00', class: '' },
+    { time: '16:30', class: '' },
+    { time: '17:00', class: '' },
+    { time: '17:30', class: '' },
+    { time: '18:30', class: '' },
+    { time: '19:00', class: '' },
+    { time: '19:30', class: '' },
+    { time: '20:00', class: '' },
+    { time: '20:30', class: '' },]
   changeColors = '';
-  changeColors1='';
+  changeColors1 = '';
   click = false;
   class;
   button;
   online = false;
   quydoi: number;
+  errorDate = false;
   listDept;
   listDoctor;
   colorTime;
-  deptId;
+  appontmentSchedule: appointmentSchedule= new appointmentSchedule();
   disableSelect = new FormControl();
   formDangKy = new FormGroup({
     phuongThuc: new FormControl(null, Validators.required),
@@ -64,7 +67,7 @@ export class RegistrationScheduleComponent implements OnInit {
   });
 
   // tslint:disable-next-line:max-line-length
-  constructor(private headerService: HeaderserviceService, private route: Router, private paymentService: PaymentService, private deptService: DeptService, private doctorService: DoctorService) {
+  constructor(private headerService: HeaderserviceService, private route: Router, private paymentService: PaymentService, private deptService: DeptService, private doctorService: DoctorService,private userService: UserserviceService) {
     deptService.getListDept().subscribe(data => {
       this.listDept = data;
     });
@@ -72,6 +75,7 @@ export class RegistrationScheduleComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.appontmentSchedule.patientid=+localStorage.getItem("id");
     this.headerService.setActive('appointment-schedule');
   }
 
@@ -91,7 +95,6 @@ export class RegistrationScheduleComponent implements OnInit {
       this.changeColors = 'change-color';
 
     }
-    this.formDangKy.controls.gioKham.setValue(300000);
     // this.formDangKy.controls['gia'].setValue(300000);
     console.log(this.formDangKy.value);
 
@@ -100,7 +103,6 @@ export class RegistrationScheduleComponent implements OnInit {
 
   clickDate() {
     this.class = 'date-registration-click';
-
   }
 
   createTime() {
@@ -108,27 +110,40 @@ export class RegistrationScheduleComponent implements OnInit {
   }
 
   payment() {
+    let date =new Date( this.formDangKy.controls.ngayKham.value);
+    let dateNow = new Date();
+    console.log(date+"  "+dateNow)
+    if (date <= dateNow) {
+      this.errorDate = true;
+    }
+    else{
+      this.errorDate = false;
+      this.appontmentSchedule.date=this.formDangKy.controls.ngayKham.value;
+      this.appontmentSchedule.time= this.formDangKy.controls.time.value;
+      this.appontmentSchedule.typeclinic=this.formDangKy.controls.phuongThuc.value;
+      Swal.fire({
+        title: 'Xác nhận thanh toán',
+        text: 'Giá tiền là: ' + this.formDangKy.controls.gia.value + 'VNĐ  ( Được quy đổi sang là:' + this.quydoi + ' $)',
+        icon: 'warning',
+        confirmButtonText: 'Xác nhận',
+        confirmButtonColor: '#3085d6',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.paymentService.getPayr(this.quydoi).subscribe(data => {
+            console.log(data);
+            window.location.href = data.linkPayment;
+          });
+        }
+      });
+    }
 
-    Swal.fire({
-      title: 'Xác nhận thanh toán',
-      text: 'Giá tiền là' + this.formDangKy.controls.gia.value + 'VNĐ  ( Được quy đổi sang là:' + this.quydoi + ' $)',
-      icon: 'warning',
-      confirmButtonText: 'Xác nhận',
-      confirmButtonColor: '#3085d6',
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.paymentService.getPayr(this.quydoi).subscribe(data => {
-          console.log(data);
-          window.location.href = data.linkPayment;
-        });
-      }
-    });
 
     //  window.location.href = 'https://www.google.com';
     // this.route.navigate(['user/payment']);
   }
 
   choseDept(deptid) {
+
     const date = this.formDangKy.controls.ngayKham.value;
     console.log(date + '' + deptid);
     this.doctorService.getListDoctorByDept(deptid, date).subscribe(data => {
@@ -138,6 +153,7 @@ export class RegistrationScheduleComponent implements OnInit {
   }
 
   doctorChange(doctorid) {
+    this.appontmentSchedule.doctorid=doctorid;
     if (this.online) {
       this.doctorService.getPriceDoctor(doctorid).subscribe(data => {
         this.formDangKy.controls.gia.setValue(data.price);
@@ -150,12 +166,13 @@ export class RegistrationScheduleComponent implements OnInit {
   }
 
   clickTime(name) {
-    console.log(name);
-    for(let item of this.time){
-      if(item.time===name){
-        item.class='btn-color';
+    console.log(this.errorDate);
+    for (let item of this.time) {
+      if (item.time === name) {
+        item.class = 'btn-color';
+        this.formDangKy.controls.gioKham.setValue(name);
       }
-      else{
+      else {
         item.class = '';
       }
     }
